c4071d920d11230b66020a7872b23e9682212c83
csc7545 <csc7545@naver.com>
2021-09-05 17:34:39 +0900

    first push

---
+++ src/com/example/lab1/Main.java
@@ -0,0 +1,13 @@
+package com.example.lab1;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Hello World!!!\n");
+		System.out.print("이름 입력: ");
+		String name = scan.next();
+		System.out.println("[" + name + "]님 환영합니다~!");
+	}
+}
